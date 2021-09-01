454ef9f2c2e7a30a089189267c18363b5996fd1a
GtzRicky <ricardogusar93@hotmail.com>
2021-08-31 19:30:44 -0500

    completed

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 //Imports de los módulos
-const readFileUsers = () => {
+
+const fs = require('fs/promises');
+const path = require('path');
+
+const usersRequirement = path.resolve('users.json');
+const writeRequeriment = path.resolve('hello.txt');
+
+const readFileUsers = async () => {
     //Imprimir en consola el arreglo de usuarios
+    try {
+        const data = await fs.readFile(usersRequirement, "utf8");
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-const writeHelloWorld = () => {
+const writeHelloWorld = async () => {
     //Escribir hello world! en el archivo hello.txt
-
+    try {
+        await fs.writeFile(writeRequeriment, "hello world!");
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 const addUser = async (username) => {
     //Agregar un usuario en la lista users.json
+    try {
+        const data = await fs.readFile(usersRequirement, "utf8");
+        const usersList = JSON.parse(data);
+        usersList.push(username);
+        await fs.writeFile(usersRequirement, JSON.stringify(usersList));
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 //No hace falta ejecutar las funciones
